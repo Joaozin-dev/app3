84d949fb477410158289bd00d3ec2dc762dd250d
Glitch (joaozin-dev-app) <none>
2020-01-09 18:31:55 +0000

    👛✨ Checkpoint ./public/quimica/js/popup.js:8798828/361 ./Views/pages/login.html:8798828/3222

--- public/quimica/js/popup.js
+++ public/quimica/js/popup.js
@@ -6,5 +6,18 @@ function PopupUserWithImage(title,text,imageUrl){
     imageWidth: 400,
     imageHeight: 200,
     imageAlt: 'User Picture',
+    position: 'bottom'
+  })
+}
+
+function Popup(title){
+  Swal.fire({
+    title,
+    showClass: {
+      popup: 'animated fadeInDown faster'
+    },
+    hideClass: {
+      popup: 'animated fadeOutUp faster'
+    }
   })
 }
