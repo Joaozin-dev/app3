e1b9c72f5c6000e30fc36820320181fd4872466e
Glitch (joaozin-dev-app) <none>
2020-01-09 14:03:38 +0000

    Import of Joaozin-dev/app2

--- config/code.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	getCode(socket){
-		const code = Math.floor(Math.random()*10000-1);
-		socket.emit('con',{code});
-		return code;
-	}
-};
--- config/sexist.js
+++
@@ -1,7 +0,0 @@
-function socketExist(arr,socket){
-	return arr.some(function(el){
-		return el.socket === socket;
-	})
-}
-
-module.exports =  socketExist;
