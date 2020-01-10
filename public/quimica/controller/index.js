aaf58f14f80c5d32635e13fb031a5d44057ac939
Glitch (joaozin-dev-app) <none>
2020-01-10 00:26:52 +0000

    🚡📯 Checkpoint ./Views/pages/login.html:8798828/12238 ./Views/controller.html:8798828/10600 ./public/quimica/controller/index.js:8798828/2698

--- public/quimica/controller/index.js
+++ public/quimica/controller/index.js
@@ -1,4 +1,30 @@
-import { ControllerAPI } from '/quimica/apis/gameapi.js';
-
-var game = new ControllerAPI();
-
+const socket = io("https://airgames.tk");
+var lastCode = "";
+document.getElementById("con").addEventListener("click", function() {
+  socket.emit("getScreen", true);
+});
+socket.on("screen-id", function(data) {
+  console.log(data);
+});
+document.getElementById("send").addEventListener("click", function() {
+  const el = document.getElementById("code");
+  const code = el.value;
+  socket.emit("new-controller", {
+    code,
+    player: {
+      user: "<%= fb%>",
+      photo: "<%= photo%>",
+      name: "<%= name%>"
+    }
+  });
+  lastCode = code;
+  el.value = "";
+});
+socket.on("remove-connection", function() {
+  alert("Tela Desconectada");
+  const el = document.getElementById("code");
+  el.value = lastCode;
+});
+function hello() {
+  return socket;
+}
--- public/quimica/controller/socket.js
+++
@@ -1,4 +0,0 @@
-import Socket from '/quimica/js/socket.js';
-const socket = new Socket('http://localhost:3000').connect();
-
-export default socket;
