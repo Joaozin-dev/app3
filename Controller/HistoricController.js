101f324278df5fc8723094d703ca703443600a62
Glitch (joaozin-dev-app) <none>
2020-01-10 22:26:40 +0000

    👁🍦 Checkpoint ./Controller/HistoricController.js:8798828/4845

--- Controller/HistoricController.js
+++ Controller/HistoricController.js
@@ -1,5 +1,6 @@
 const HistoricModel = require("../Model/HistoricModel");
 const UserModel = require("../Model/UserModel");
+const GameModel = require("../Model/GameModel");
 const axios = require("axios");
 module.exports = {
   async show(req, res) {
@@ -14,6 +15,7 @@ module.exports = {
         if (item.usuarios_user_id === userID) {
           UserModel.findAll({
             attributes: [
+              "user_id",
               "facebook_id",
               "user_nome",
               "user_email",
@@ -22,7 +24,28 @@ module.exports = {
             ]
           }).then(query => {
             const users = JSON.parse(JSON.stringify(query));
-            console.log(query);
+            users.forEach(user => {
+              if (user.user_id === userID) {
+                response = { ...response, user };
+              }
+            });
+          });
+          GameModel.findAll({
+            attributes: [
+              "game_id",
+              "game_name",
+              "game_picture",
+              "game_description",
+              "game_last_update",
+              "game_price"
+            ]
+          }).then(query => {
+            const users = JSON.parse(JSON.stringify(query));
+            users.forEach(user => {
+              if (user.user_id === userID) {
+                response = { ...response, user };
+              }
+            });
           });
         } else {
         }
