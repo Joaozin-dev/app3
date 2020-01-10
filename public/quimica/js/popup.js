9c25a8f041d68a08961f314c9b747848d0a13351
Glitch (joaozin-dev-app) <none>
2020-01-10 16:23:26 +0000

    🐄🚞 Checkpoint ./public/quimica/js/popup.js:8798828/1107 ./Views/controller.html:8798828/628 ./Views/pages/info.html:8798828/2412

--- public/quimica/js/popup.js
+++ public/quimica/js/popup.js
@@ -27,14 +27,21 @@ function PopupUserWithImage(title,text,imageUrl){
   })
 }
 
-function Popup(title){
+function PopupCode(){
   Swal.fire({
-    title,
-    showClass: {
-      popup: 'animated fadeInDown faster'
-    },
-    hideClass: {
-      popup: 'animated fadeOutUp faster'
-    }
-  })
+    title: '<strong>HTML <u>example</u></strong>',
+    icon: 'info',
+    html:
+      `<input type="number" placeholder="Seu Codigo">`+
+      `<button class="btn">Enviar</button>`,
+    showCloseButton: true,
+    showCancelButton: true,
+    focusConfirm: false,
+    confirmButtonText:
+      '<i class="fa fa-thumbs-up"></i> Great!',
+    confirmButtonAriaLabel: 'Thumbs up, great!',
+    cancelButtonText:
+      '<i class="fa fa-thumbs-down"></i>',
+    cancelButtonAriaLabel: 'Thumbs down'
+  });
 }
