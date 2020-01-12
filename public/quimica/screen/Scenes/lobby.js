a52aa900ce795cb8ef2043a3c07f5a3a27825cd2
Glitch (joaozin-dev-app) <none>
2020-01-12 14:41:58 +0000

    😄🍘 Checkpoint ./public/quimica/gameviewer.html:8798828/65 ./public/quimica/screen/Scenes/lobby.js:8798828/800

--- public/quimica/screen/Scenes/lobby.js
+++ public/quimica/screen/Scenes/lobby.js
@@ -8,13 +8,9 @@ class Lobby extends Phaser.Scene{
         console.log(this.game);
         this.add.text(100,100,`WAITING FOR PLAYING`,{fontFamily: 'Arial'});
         this.gameapi.onReady((device_id)=>{
-            this.gameapi.onMessage((controller_id,message)=>{
-                if(message.action === 'start'){
-                    this.scene.start('Fase1');
-                } else if(message.action === 'add_new_player'){
-                    alert(`Player: ${message.player}`);
-                }
-            })
+          this.gameapi.onPlayerConnect((player_id)=>{
+            console.log(`PLAYER {${player_id}} CONECTADO MSG DO LOBBY.JS`);
+          });
         });
     }
     update(){}
