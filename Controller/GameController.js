ea5126852d6b9a2f20005e9345b5f0aff84fb907
Glitch (joaozin-dev-app) <none>
2020-01-13 15:19:44 +0000

    ⛹🍬 Checkpoint ./Views/pages/play.html:8798828/40 ./Views/pages/login.html:8798828/64 ./Views/pages/info.html:8798828/108 ./Views/pages/historic.html:8798828/183 ./Controller/GameController.js:8798828/14

--- Controller/GameController.js
+++ Controller/GameController.js
@@ -3,7 +3,7 @@ const GameModel = require('../Model/GameModel');
 module.exports = {
   index(req,res){
     GameModel.findAll({
-      attributes: ['game_id','game_name','game_picture','game_description','game_last_update','game_price']
+      attributes: ['game_id','game_id_dev','game_name','game_picture','game_description','game_last_update','game_price']
     }).then((query)=>{
       const data = JSON.parse(JSON.stringify(query));
       res.header('content-type','application/json');
